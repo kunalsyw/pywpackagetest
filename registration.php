88cae6056b25eb050a8856a1030e4240bf7e71da
Test <abc@test.com>
2022-01-06 19:54:33 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author    Blue Acorn iCi <code@blueacornici.com>
+ * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'PayYourWay_Pyw', __DIR__);
